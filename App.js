1893f56cf2bcc7d7a6135c58e39c5f8eb352bb5f
Student <student@Admins-iMac.local>
2019-02-27 09:26:38 -0600

    stash

--- App.js
+++ App.js
@@ -1,21 +1,11 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import { Text, View, } from 'react-native';
+import Login from '/Users/student/Desktop/rmm/FrontEnd-RMM/srcl/components/Login.js'
 
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+        < Login/>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

9a1f792583429444913d88f75a6495ca95e30f5b
joshua-hurn <joshua.hurn@hotmail.com>
2019-02-27 14:15:39 -0600

    making shit up

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
 import { Text, View, } from 'react-native';
-import Login from './srcl/components/Login.js'
+import ClassCard from './src/components/ClassCard';
 
 export default class App extends Component {
   render() {
     return (
-        < Login/>
+        < ClassCard/>
     );
   }
 }

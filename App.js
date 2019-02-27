cbac339d949dbf4eb40716f01b7ecd4a56b3fdd7
Lee Panter <lithelli@Lees-MacBook-Pro.local>
2019-02-27 14:49:59 -0600

    changes

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Text, View, } from 'react-native';
 import Login from './srcl/components/Login.js'
+import ClassCard from './src/components/ClassCard'
 
 export default class App extends Component {
   render() {

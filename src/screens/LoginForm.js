2611b46e09bc7657801d12ed22c07a429356e6f2
Michael Banks <michaelbanks@Michaels-MacBook-Pro-2.local>
2019-03-02 17:56:33 -0600

    Testing Api functionality

--- src/screens/LoginForm.js
+++ src/screens/LoginForm.js
@@ -2,87 +2,121 @@ import React, { Component } from 'react'
 import { TextInput, StyleSheet, Button, KeyboardAvoidingView, Image, SafeAreaView, StatusBar } from 'react-native';
 
 export default class LoginForm extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            email: "",
-            password: ""
-        }
-    };
-    forgot = () => {
-      return
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+      isLoggingIn: false
     }
-    render() {
-      const {navigate} = this.props.navigation;
-        return (
+  };
 
-            <SafeAreaView>
-                <KeyboardAvoidingView behavior="padding" style={styles.container}>
-                    <StatusBar 
-                        barStyle="light-content"
-                    />
-                    <Image
-                        source={require('../../assets/RedLogo.png')}
-                        style={styles.img}
-                        />
-                    <TextInput
-                        style={styles.input}
-                        onChangeText={(text) => this.setState({ email: text })}
-                        placeholder="E-Mail"
-                        returnKeyType="next"
-                        keyboardType="email-address"
-                        autoCapitalize="none"
-                        autoCorrect={false}
-                        onSubmitEditing={() => this.passwordInput.focus()}
-                    />
-                    <TextInput
-                        style={styles.input}
-                        onChangeText={(text) => this.setState({ password: text })}
-                        secureTextEntry={true}
-                        placeholder="Password"
-                        returnKeyType="go"
-                        ref={(input) => this.passwordInput = input}
-                    />
-                    <Button
-                        onPress={() => navigate('App')}
-                        title="Login"
-                        color="#b70303"
-                        ref={(login) => this.loginButton = login}
-                    />
-                    <Button
-                        onPress={this.forgot}
-                        title="Forgot Password?"
-                        color="#b70303"
-                    />
-                </KeyboardAvoidingView>
-            </SafeAreaView>
-        );
-    }
+  userLogin = () => {
+    this.setState({ isLoggingIn: true });
+    let proceed = false;
+    fetch("http://localhost:3000/api/login", {
+      method: "POST",
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email: this.state.email,
+        password: this.state.password
+      }),
+    }).then((res) => res.text())
+      .then((resData) => {
+        if (resData=='home page') {
+          proceed = true;
+          console.log(resData);
+        } else {
+          console.log('no')
+        }
+      })
+      .then(() => {
+        this.setState({ isLoggingIn: false })
+        if (proceed) this.props.navigation.navigate('App');
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  forgot = () => {
+    return
+  }
+
+  render() {
+    // const { navigate } = this.props.navigation;
+    return (
+      <SafeAreaView>
+        <KeyboardAvoidingView behavior="padding" style={styles.container}>
+          <StatusBar
+            barStyle="light-content"
+          />
+          <Image
+            source={require('../../assets/RedLogo.png')}
+            style={styles.img}
+          />
+          <TextInput
+            ref={input => this.email = input}
+            style={styles.input}
+            onChangeText={(email) => this.setState({ email })}
+            placeholder="Email"
+            returnKeyType="next"
+            keyboardType="email-address"
+            autoCapitalize="none"
+            autoCorrect={false}
+          />
+          <TextInput
+            ref={input => this.password = input}
+            style={styles.input}
+            onChangeText={(password) => this.setState({ password })}
+            onSubmitEditing={this.userLogin}
+            secureTextEntry={true}
+            placeholder="Password"
+            returnKeyType="go"
+          />
+          <Button
+            disabled={this.state.isLoggingIn || !this.state.email || !this.state.password}
+            onPress={this.userLogin}
+            title="Login"
+            color="#b70303"
+          />
+          <Button
+            onPress={this.forgot}
+            title="Forgot Password?"
+            color="#b70303"
+          />
+        </KeyboardAvoidingView>
+      </SafeAreaView>
+    );
+  }
 }
 
- const styles = StyleSheet.create({
-    input: {
-        color: "#b70303",
-        textAlign: "left",
-        width: 250,
-        margin: 5,
-        fontWeight: "bold",
-        paddingTop: 95,
-        borderBottomWidth: 1,
-        borderBottomLeftRadius: 3,
-    },
-    container: {
-        flex:1
-    },
-    login: {
-        textAlign: "right",
-    },
+const styles = StyleSheet.create({
+  input: {
+    color: "#b70303",
+    textAlign: "left",
+    width: 250,
+    margin: 5,
+    fontWeight: "bold",
+    paddingTop: 95,
+    borderBottomWidth: 1,
+    borderBottomLeftRadius: 3,
+  },
+  container: {
+    flex: 1
+  },
+  login: {
+    textAlign: "right",
+  },
 
-    forgot: {
-        textAlign: "center",
-    },
-    img: {
-        margin: 20,
-        alignSelf:"center"
-    }
+  forgot: {
+    textAlign: "center",
+  },
+  img: {
+    margin: 20,
+    alignSelf: "center"
+  }
 });

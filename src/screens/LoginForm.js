ce41ca7ebab7e630695d20255eb65d8e08401e65
Lithelli <leepatner@ymail.com>
2019-03-03 16:42:06 -0600

    whats up

--- src/screens/LoginForm.js
+++ src/screens/LoginForm.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react'
-import { TextInput, StyleSheet, KeyboardAvoidingView, Image, View, SafeAreaView, StatusBar, ScrollView } from 'react-native';
-import { Button } from 'react-native-elements'
-export default class LoginForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: "",
-      password: "",
-      isLoggingIn: false
-    }
-  };
+import { StyleSheet, KeyboardAvoidingView, SafeAreaView, TextInput, Image } from 'react-native';
+import { Button, Input } from 'react-native-elements'
+// export default class LoginForm extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       email: "",
+//       password: "",
+//       isLoggingIn: false
+//     }
+//   };
 
   // userLogin = () => {
   //   this.setState({ isLoggingIn: true });
@@ -42,64 +42,64 @@ export default class LoginForm extends Component {
   //       console.log(err);
   //     });
   // }
-  login = () => {
-    this.props.navigation.navigate('App')
-  }
-  forgot = () => {
-    return
-  }
+//   login = () => {
+//     this.props.navigation.navigate('App')
+//   }
+//   forgot = () => {
+//     return
+//   }
 
-  render() {
-    const { navigate } = this.props.navigation;
-    return (
-      <ScrollView>
-        <KeyboardAvoidingView behavior="padding" enabled>
-            <Image
-              source={require('../../assets/RedLogo.png')}
-              style={styles.img}
-            />
-            <Button
-              title="Sign Up"
-              onPress={this.SignUp}
-              type="clear"
-            />
-            <TextInput
-              ref={input => this.email = input}
-              style={styles.input}
-              onChangeText={(email) => this.setState({ email })}
-              placeholder="Email"
-              returnKeyType="next"
-              keyboardType="email-address"
-              onSubmitEditing={() => this.password.focus()}
-              autoCapitalize="none"
-              autoCorrect={false}
-            />
-            <TextInput
-              ref={input => this.password = input}
-              style={styles.input}
-              onChangeText={(password) => this.setState({ password })}
-              onSubmitEditing={this.userLogin}
-              secureTextEntry={true}
-              placeholder="Password"
-              returnKeyType="go"
-            />
-            <Button
-              // disabled={this.state.isLoggingIn || !this.state.email || !this.state.password}
-              // onPress={this.userLogin}
-              onPress={this.login}
-              title="Login"
-              type="clear"
-            />
-            <Button
-              onPress={this.forgot}
-              title="Forgot Password?"
-              type="clear"
-            />
-        </KeyboardAvoidingView>
-      </ScrollView>
-    );
-  }
-}
+//   render() {
+//     const { navigate } = this.props.navigation;
+//     return (
+//       <ScrollView>
+//         <KeyboardAvoidingView behavior="padding" enabled>
+//             <Image
+//               source={require('../../assets/RedLogo.png')}
+//               style={styles.img}
+//             />
+//             <Button
+//               title="Sign Up"
+//               onPress={this.SignUp}
+//               type="clear"
+//             />
+//             <TextInput
+//               ref={input => this.email = input}
+//               style={styles.input}
+//               onChangeText={(email) => this.setState({ email })}
+//               placeholder="Email"
+//               returnKeyType="next"
+//               keyboardType="email-address"
+//               onSubmitEditing={() => this.password.focus()}
+//               autoCapitalize="none"
+//               autoCorrect={false}
+//             />
+//             <TextInput
+//               ref={input => this.password = input}
+//               style={styles.input}
+//               onChangeText={(password) => this.setState({ password })}
+//               onSubmitEditing={this.userLogin}
+//               secureTextEntry={true}
+//               placeholder="Password"
+//               returnKeyType="go"
+//             />
+//             <Button
+//               // disabled={this.state.isLoggingIn || !this.state.email || !this.state.password}
+//               // onPress={this.userLogin}
+//               onPress={this.login}
+//               title="Login"
+//               type="clear"
+//             />
+//             <Button
+//               onPress={this.forgot}
+//               title="Forgot Password?"
+//               type="clear"
+//             />
+//         </KeyboardAvoidingView>
+//       </ScrollView>
+//     );
+//   }
+// }
 
 const styles = StyleSheet.create({
   input: {
@@ -108,10 +108,9 @@ const styles = StyleSheet.create({
     width: 250,
     margin: 5,
     fontWeight: "bold",
-    paddingTop:70,
     borderBottomWidth: 1,
     flexGrow: 1,
-    borderBottomLeftRadius: 3,
+    borderBottomLeftRadius: 3
   },
   container: {
     flexGrow: 1
@@ -128,3 +127,62 @@ const styles = StyleSheet.create({
     alignSelf: "center"
   }
 });
+
+
+
+export default class LoginForm extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+     email: "",
+     password: "",
+     isLoggingIn: false
+     }
+  }
+
+  login = () => {
+    this.props.navigation.navigate('App')
+  }
+
+  render(){
+    const { navigate } = this.props.navigation
+    return(
+      <KeyboardAvoidingView behavior="padding" style={styles.container}>
+      <SafeAreaView>
+      <Image
+               source={require('../../assets/RedLogo.png')}
+               style={styles.img}
+             />
+         <Input
+             ref={input => this.email = input}
+             onChangeText={(email) => this.setState({ email })}
+             placeholder="Email"
+             style={styles.input}
+             returnKeyType="next"
+             secureTextEntry = {true}
+             onSubmitEditing={() => this.password.focus()}
+             autoCapitalize="none"
+             autoCorrect={false}
+          />
+          <Input
+             ref={input => this.password = input}
+             style={styles.input}
+             onChangeText={(password) => this.setState({ password })}
+             placeholder="Password"
+             returnKeyType="go"
+             keyboardType="email-address"
+             autoCapitalize="none"
+             autoCorrect={false}
+          />
+          <Button
+            // disabled={this.state.isLoggingIn || !this.state.email || !this.state.password}
+            // onPress={this.userLogin}
+            onPress={this.login}
+            title="Login"
+            type="clear"
+          />
+      </SafeAreaView>
+      </KeyboardAvoidingView>
+    );
+  }
+}

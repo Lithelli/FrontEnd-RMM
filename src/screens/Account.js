28321211a64bdf6c17de40c4366683edac86e280
joshua-hurn <joshua.hurn@hotmail.com>
2019-03-07 12:55:16 -0600

    Merging

--- src/screens/Account.js
+++ src/screens/Account.js
@@ -36,10 +36,6 @@ export default class Home extends Component {
   //   let marginActual = getStatusBarHeight();
   //   this.setState({ statusBarMargin: marginActual });
   // }
-  
-  renderItem(item) {
-    return <ListItem item={item} />;
-  }
 
   render() {
     return (
@@ -62,7 +58,7 @@ export default class Home extends Component {
           <View style={{ flex: 1 }}>
             <FlatList
               data={this.state.list}
-              renderItem={this.renderItem}
+              renderItem={({ item }) => <ListItem item={item} />}
               keyExtractor={item => item.id}
             />
           </View>

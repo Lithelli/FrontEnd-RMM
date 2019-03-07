7908be9537e0250a0a94b4b4a3ae866e79c8f5e9
joshua-hurn <joshua.hurn@hotmail.com>
2019-03-07 11:21:13 -0600

    incomplete account page

--- src/screens/Account.js
+++ src/screens/Account.js
@@ -1,57 +1,47 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, ScrollView, SafeAreaView } from 'react-native';
-import { Avatar, Text, Card, ListItem } from 'react-native-elements';
-import { getStatusBarHeight } from 'react-native-status-bar-height';
-let console = require('console');
+import { View, StyleSheet, ScrollView, SafeAreaView, FlatList } from 'react-native';
+import { Avatar, Text } from 'react-native-elements';
+// import { getStatusBarHeight } from 'react-native-status-bar-height';
+import ListItem from '../components/ListItem';
 
 export default class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      userName: "DaveChopWood",
-      membershipStatus: "Full Member",
+      userName: 'DaveChopWood',
+      membershipStatus: 'Full Member',
       statusBarMargin: 0,
-      collapse: false
-    }
+      collapse: true,
+      list: [
+        {
+          id: '1',
+          title: 'My Classes',
+          icon: 'class',
+        },
+        {
+          id: '2',
+          title: 'Membership',
+          icon: 'credit-card',
+        },
+        {
+          id: '3',
+          title: 'Account Settings',
+          icon: 'settings',
+        },
+      ]
+    };
   }
 
-  handleStatusBarMargin = () => {
-    let marginActual = getStatusBarHeight();
-    this.setState({ statusBarMargin: marginActual });
-  }
-
-  handleListItemStyle = () => {
-    this.setState({
-      collapse: !this.state.collapse
-    })
+  // handleStatusBarMargin = () => {
+  //   let marginActual = getStatusBarHeight();
+  //   this.setState({ statusBarMargin: marginActual });
+  // }
+  
+  renderItem(item) {
+    return <ListItem item={item} />;
   }
 
   render() {
-    const list = [
-      {
-        title: 'My Classes',
-        icon: 'class',
-        content: {
-          membershipStatus: this.state.membershipStatus,
-          level_of_dopeness: 3.50
-        }
-      },
-      {
-        title: 'Membership',
-        icon: 'credit-card', content: {
-          membershipStatus: this.state.membershipStatus,
-          level_of_dopeness: 3.50
-        }
-      },
-      {
-        title: 'Account Settings',
-        icon: 'settings',
-        content: {
-          membershipStatus: this.state.membershipStatus,
-          level_of_dopeness: 3.50
-        }
-      },
-    ]
     return (
       <ScrollView>
         <SafeAreaView>
@@ -64,38 +54,21 @@ export default class Home extends Component {
               <View style={styles.avatar}>
                 <Avatar
                   size='large'
-                  rounded
-                  source={{
-                    uri: '{https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjFu4qF4e3gAhVInOAKHQLcDn4QjRx6BAgBEAU&url=https%3A%2F%2Fwww.redmountainmakers.org%2F&psig=AOvVaw16bFNBBIgy0uKyZ93YFNtf&ust=1551970026776554}'
-                  }}
-                  showEditButton
+                  source={{ uri: 'https://yt3.ggpht.com/a-/AAuE7mC8fd9OfGF-LE7viS4c1CLtKwmMaN0j6vqIUg=s288-mo-c-c0xffffffff-rj-k-no' }}
                 />
               </View>
             </View>
           </View>
           <View style={{ flex: 1 }}>
-              {
-                list.map((item, index) => (
-                  <Card
-                    key={index}
-                    style={styles.infoCards}
-                    title={item.title}
-                    onPress={this.handleListItemStyle}
-                  >
-                    {
-                      this.state.collapse ?
-                        <View>
-                          <Text>{item.content}</Text>
-                        </View>
-                        : null
-                    }
-                  </Card>
-                ))
-              }
-            </View>
+            <FlatList
+              data={this.state.list}
+              renderItem={this.renderItem}
+              keyExtractor={item => item.id}
+            />
+          </View>
         </SafeAreaView>
-      </ScrollView >
-    )
+      </ScrollView>
+    );
   }
 }
 
@@ -103,14 +76,14 @@ const styles = StyleSheet.create({
   container: {
     marginTop: 24,
     flex: 1,
-    justifyContent: "center"
+    justifyContent: 'center'
   },
   infoCards: {
     margin: 0,
     padding: 0
   },
   text: {
-    textAlign: "center",
+    textAlign: 'center',
   },
   avatar: {
     paddingTop: 20,
